8da87b8a20b001bf4f649c9e0dcc8ecf55ec1dda
Marc Ruemekorf <marc@users.noreply.github.com>
2025-01-15 08:11:38 +0100

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,18 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {sourceType: "commonjs"}
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      indent: ["error", "tab"],
+      "prettier/prettier": ["error", { useTabs: true }]
+    }
+  }
 ];
